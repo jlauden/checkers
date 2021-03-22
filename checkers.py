89b7c2b1205ea8844a85a69358579893f728d2e5
jlauden <37051917+jlauden@users.noreply.github.com>
2021-03-21 22:34:00 -0400

    Add files via upload
    Progress so far:
     - Board drawn
     - Detect which square is clicked
     - Test game piece drawn

---
+++ checkers.py
@@ -0,0 +1,103 @@
+import tkinter as tk
+
+def gamepiece_click(event):
+
+    # Determine which square was clicked
+    horizontal_square_pos = (event.x - width_buffer) // square_edge_len
+    vertical_square_pos = (event.y - height_buffer) // square_edge_len
+
+    # get clicked square object
+    clicked_square = squares[str(horizontal_square_pos) + str(vertical_square_pos)]
+    canvas.itemconfig(clicked_square, fill="blue")
+    
+
+    #if (event.x > 0 and event.x < 50) and (event.y > 0 and event.y < 50):
+    #    canvas.itemconfig(gamepiece, fill="white")
+
+# -------- VARIABLES ---------------
+# Board
+square_edge_len = 50
+checker_dia = 40
+squares_per_row = 8
+
+# Window
+width_buffer = 50
+height_buffer = 100
+window_width = (square_edge_len*squares_per_row)+(2*width_buffer)
+window_height = (square_edge_len*squares_per_row)+(2*height_buffer)
+
+
+
+# --------- MAIN SCRIPT --------------
+# create window
+window = tk.Tk()
+window.geometry(str(window_width) +"x"+ str(window_height))
+window.title("Jon's Amateur Checkers")
+
+# create canvas
+canvas = tk.Canvas(
+    master=window,
+    width=600,
+    height=500,
+    relief="raised",
+    )
+
+# CREATE BOARD
+red = True  # flag for square color
+squares = {}  # dict to store squares
+
+for i in range(squares_per_row):
+    for j in range(squares_per_row):
+
+        # determine square color
+        if red:
+            square_color = "red"
+        else:
+            square_color = "black"
+
+        # create and store square in dict
+        squares[str(i)+str(j)] = canvas.create_rectangle(
+            width_buffer + j*square_edge_len,
+            height_buffer + i*square_edge_len,
+            width_buffer + (j+1)*square_edge_len,
+            height_buffer + (i+1)*square_edge_len,
+            fill=square_color,
+            outline = "yellow"
+            )
+
+        # invert square color for next iteration
+        # skip last square to get alternating pattern
+        if j < (squares_per_row-1):
+            red = not red
+
+# draw pieces
+gamepiece = canvas.create_oval(
+    width_buffer,
+    height_buffer,
+    width_buffer + checker_dia,
+    height_buffer + checker_dia,
+    fill = 'grey'
+    )
+
+canvas.bind("<Button-1>", gamepiece_click)
+
+# pack it up
+canvas.pack()
+
+
+
+# main loop with listeners, etc
+window.mainloop()
+
+
+
+# add text
+#greeting = tk.Label(text = "Hello World!")
+#greeting.pack()
+
+# add button
+#button = tk.Button(master=window, text="Click Me", command=click_test)
+#button.pack()
+
+# add oval to canvas
+#oval = canvas.create_oval(10, 10, 40, 40)
