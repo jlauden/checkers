ef668b4bad183e71efe8f11ddef13696b44c5788
jlauden <37051917+jlauden@users.noreply.github.com>
2021-03-28 00:21:17 -0400

    Add files via upload
    - Computer player improvements
         - able to jump users pieces
         - takes jump moves if possible
    - Fixed bug allowing jumping one's own piece

--- checkers.py
+++ checkers.py
@@ -129,6 +129,9 @@ def valid_move(piece_position, target_position):
     Returns true if the attempted move is valid
     '''
 
+    # Assume false
+    validity = False
+
     # Target position may not be negative
     if '-' in target_position:
         return False
@@ -153,6 +156,7 @@ def valid_move(piece_position, target_position):
     # Moves must be diagonal
     x_diff = abs(piece_x - target_x)
 
+    # Account for different y direction for comp vs user
     if piece_color(piece_position) == red_piece_color:
         y_diff = target_y - piece_y
     else:
@@ -166,10 +170,18 @@ def valid_move(piece_position, target_position):
         validity = True
     # or if jump, two spaces diagonally over opponent piece
     elif x_diff == 2 and y_diff == 2:
-        middle_position = str(x_diff//2) + str(y_diff//2)
-        if piece_color(middle_position) != piece_color(piece_position):
-            validity = True
-        
+        middle_position = str((piece_x + target_x)//2) + str((piece_y + target_y)//2)
+        #print("Piece position is " + piece_position)
+        #print("middle position is " + middle_position)
+        #print("middle piece color is " + piece_color(middle_position))
+        #print("moved piece color is " + piece_color(piece_position))
+        if board[middle_position] != "":
+            if piece_color(middle_position) == piece_color(piece_position) and piece_color(piece_position) == red_piece_color:
+                return False
+            elif selected_piece and piece_color(middle_position) == black_piece_color:  
+                return False
+            else:
+                validity = True
     else:
         #print("problem with target square's distance from piece")
         return False
@@ -179,36 +191,38 @@ def valid_move(piece_position, target_position):
     return validity
 
 def computer_move():
-
+    #print("my turn!")
     #if not user_turn:
-    for position in board:
-        gamepiece = board[position]
-        #print(board)
-        #print(gamepiece)
-        if gamepiece != "":
-            #print("I found a piece to move!")
-            if canvas.itemcget(gamepiece, "fill") == red_piece_color:
-                #print("and it's red")
-                # options to move piece
-                x1 = int(position[0]) + 1
-                x2 = int(position[0]) - 1
-                y = int(position[1]) + 1
+    #print("my turn! 2")
+    # Check for jumps first, then single moves
+    for offset in [2, -2, 1, -1]:
+        for position in board:
+            gamepiece = board[position]
+            #print(board)
+            #print(gamepiece)
+            if gamepiece != "":
+                #print("I found a piece to move!")
+                if canvas.itemcget(gamepiece, "fill") == red_piece_color:
+                    #print("and it's red")
+                    # options to move piece
 
-                target1 = str(x1) + str(y)
-                target2 = str(x2) + str(y)
+                    x = int(position[0])
+                    y = int(position[1])
+                    
+                    target_x = x + offset
+                    target_y = y + abs(offset)
 
-                # Check for valid moves
-                if valid_move(position, target1):
-                    #print("computer moving piece " + position + " to " + target1)
-                    move_piece(position, target1)
-                    #global user_turn
-                    #user_turn = True
-                    return
-                elif valid_move(position, target2):
-                    move_piece(position, target2)
-                    #global user_turn
-                    #user_turn = True
-                    return
+                    # Check for valid move
+                    target_position = str(target_x) + str(target_y)
+                    if valid_move(position, target_position):
+                        if abs(offset) == 2:
+                            jump(position, target_position)
+                            return
+                        else:
+                            move_piece(position, target_position)
+                            return
+                            
+            
 
     
 
@@ -256,6 +270,7 @@ def click(event):
             # Computer's turn!
             #print("it's my turn!")
             computer_move()
+            #user_turn = False
 
 
 # -------- VARIABLES ---------------
@@ -294,6 +309,7 @@ canvas = tk.Canvas(
 red = True  # flag for square color
 squares = {}  # dict to store square objects
 board = {}  # dict to store piece locations
+user_turn = True
 
 for i in range(squares_per_row):
     for j in range(squares_per_row):
@@ -336,7 +352,7 @@ canvas.bind("<Button-1>", click)
 canvas.pack()
 
 # trying something
-#window.after(
+#window.after(1000, computer_move)
 
 
 # main loop with listeners, etc
